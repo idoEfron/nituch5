3217998bba15abaf67e229bf888fb5d336a7aa27
safamSaol <yosi_dekel@walla.com>
2020-01-18 22:51:30 +0200

    ido nutuch5

---
+++ src/ComplexState.java
@@ -0,0 +1,16 @@
+public class ComplexState extends State {
+//    // THIS IS THE CURRENT STATE
+//    public State state;
+//
+//    // IN EVERY TRANSITION, ACTIVATE ENTRY AND EXIT
+//    public void setState(State st){
+//        if(state != null)
+//            state.exit();
+//        this.state = st;
+//        state.entry();
+//    }
+//
+//    public State getState(){
+//        return this.state;
+//    }
+}
---
+++ src/State.java
@@ -0,0 +1,35 @@
+public abstract class State {
+    //COMMON METHODS
+//    public void entry(){
+//        System.out.print("");
+//    }
+//
+//    public void exit(){
+//        System.out.print("");
+//    }
+//
+//    // METHODS THAT NEED TO BE OVERRIDED TO USE:
+//    public void cardInserted(){
+//        System.out.println("error");
+//    }
+//
+//    public void abort(){
+//        System.out.println("error");
+//    }
+//
+//    public void ready(){
+//        System.out.print("error");
+//    }
+//
+//    public void readCard(){
+//        System.out.print("error");
+//    }
+//
+//    public void ejectCard(){
+//        System.out.print("error");
+//    }
+//
+//    public void transfer(int x){
+//        System.out.print("error");
+//    }
+}
